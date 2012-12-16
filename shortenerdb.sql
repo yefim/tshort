84be1a4ca7d7dfe2d196301325644c6897e01619
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-12-16 14:25:47 -0500

    Indented query

--- shortenerdb.sql
+++ shortenerdb.sql
@@ -1,1 +1,9 @@
-CREATE TABLE shorturl (id INTEGER PRIMARY KEY, keyword VARCHAR(20) NOT NULL UNIQUE, url VARCHAR(200) NOT NULL, hits INTEGER, created_at TIMESTAMP, updated_at TIMESTAMP, lock_revision INTEGER);
+CREATE TABLE shorturl (
+  id INTEGER PRIMARY KEY,
+  keyword VARCHAR(20) NOT NULL UNIQUE,
+  url VARCHAR(200) NOT NULL,
+  hits INTEGER,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  lock_revision INTEGER
+);
